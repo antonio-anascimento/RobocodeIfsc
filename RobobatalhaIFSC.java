5a970711041a0c5dcdeaf20e7e2c3df855971fd6
antonio-anascimento <antonio.a2007@aluno.ifsc.edu.br>
2025-10-23 19:29:52 -0300

    Iniciando a movimentação em onda

--- RobobatalhaIFSC.java
+++ RobobatalhaIFSC.java
@@ -1,57 +1,29 @@
 package roboifsc;
 import robocode.*;
-//import java.awt.Color;
+import java.awt.Color;
 
-// API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
-
-/**
- * RobobatalhaIFSC - a robot by (your name here)
- */
-public class RobobatalhaIFSC extends Robot
-{
-	/**
-	 * run: RobobatalhaIFSC's default behavior
-	 */
+public class RobobatalhaIFSC extends AdvancedRobot {
+	double energiaInicial = 100;
 	public void run() {
-		// Initialization of the robot should be put here
-
-		// After trying out your robot, try uncommenting the import at the top,
-		// and the next line:
-
-		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
-
-		// Robot main loop
+		setColors(Color.red,Color.blue,Color.blue); 
+		setAdjustGunForRobotTurn(true);
+		setAdjustRadarForGunTurn(true);
 		while(true) {
-			// Replace the next 4 lines with any behavior you would like
-			ahead(100);
-			turnGunRight(360);
-			back(100);
-			turnGunRight(360);
+			turnRadarRight(360);
 		}
 	}
 
-	/**
-	 * onScannedRobot: What to do when you see another robot
-	 */
 	public void onScannedRobot(ScannedRobotEvent e) {
-		// Replace the next line with any behavior you would like
-		fire(1);
-	}
+		double quedaDeEnergia = energiaInicial - e.getEnergy();
 
-	/**
-	 * onHitByBullet: What to do when you're hit by a bullet
-	 */
-	public void onHitByBullet(HitByBulletEvent e) {
-		// Replace the next line with any behavior you would like
-		back(10);
+		if (quedaDeEnergia>0&&quedaDeEnergia<=3) {
+			setTurnRight(e.getBearing()+90+(Math.random()*90-45));
+			setAhead(150*(Math.random()>0.5?1 : -1));
+		}
+
+		double radarLock = getHeading() - getRadarHeading() + e.getBearing();
+		setTurnRadarRight(radarLock);
+		execute();
 	}
-	
-	/**
-	 * onHitWall: What to do when you hit a wall
-	 */
-	public void onHitWall(HitWallEvent e) {
-		// Replace the next line with any behavior you would like
-		back(20);
-	}	
 }
-
+	
