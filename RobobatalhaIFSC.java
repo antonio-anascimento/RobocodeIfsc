316518d0ec07509c5cae54f792f9622c71773170
antonio-anascimento <antonio.a2007@aluno.ifsc.edu.br>
2025-10-23 19:40:42 -0300

    Destravando a movimentação

--- RobobatalhaIFSC.java
+++ RobobatalhaIFSC.java
@@ -1,29 +1,36 @@
 package roboifsc;
+
 import robocode.*;
 import java.awt.Color;
 
 public class RobobatalhaIFSC extends AdvancedRobot {
-	double energiaInicial = 100;
-	public void run() {
-		setColors(Color.red,Color.blue,Color.blue); 
-		setAdjustGunForRobotTurn(true);
-		setAdjustRadarForGunTurn(true);
-		while(true) {
-			turnRadarRight(360);
-		}
-	}
+    double energiaAnterior = 100;
 
-	public void onScannedRobot(ScannedRobotEvent e) {
-		double quedaDeEnergia = energiaInicial - e.getEnergy();
+    public void run() {
+        setColors(Color.red, Color.blue, Color.blue);
+        setAdjustGunForRobotTurn(true);
+        setAdjustRadarForGunTurn(true);
 
-		if (quedaDeEnergia>0&&quedaDeEnergia<=3) {
-			setTurnRight(e.getBearing()+90+(Math.random()*90-45));
-			setAhead(150*(Math.random()>0.5?1 : -1));
-		}
+        while (true) {
+            setTurnRadarRight(360);
+            execute();
+        }
+    }
 
-		double radarLock = getHeading() - getRadarHeading() + e.getBearing();
-		setTurnRadarRight(radarLock);
-		execute();
-	}
+    public void onScannedRobot(ScannedRobotEvent e) {
+        double quedaDeEnergia = energiaAnterior - e.getEnergy();
+        energiaAnterior = e.getEnergy();
+
+        if (quedaDeEnergia > 0 && quedaDeEnergia <= 3) {
+            setTurnRight(e.getBearing() + 90 + (Math.random() * 90 - 45));
+            setAhead(150 * (Math.random() > 0.5 ? 1 : -1));
+        }
+
+        double radarLock = getHeading() - getRadarHeading() + e.getBearing();
+        setTurnRadarRight(radarLock);
+
+        execute();
+    }
 }
+
 	
